244c5f0ec9576a0fd496d2a5993de925cfc0a4f5
lolMANFREDI04 <lolMANFREDI04ok@gmail.com>
2024-04-10 00:15:19 +0200

    new file:   api.css 	modified:   api.html 	modified:   api.js 	modified:   db.json

--- api.js
+++ api.js
@@ -1,11 +1,11 @@
+var pager={};
 
-
-var page=1;
+var ff=0;
 
 getTitle();
-// getAll();
 
 function getTitle(){
+
   const xhr = new XMLHttpRequest();
   xhr.open("GET", "http://localhost:3000/comments");
   xhr.send();
@@ -14,18 +14,32 @@ function getTitle(){
     if (xhr.readyState == 4 && xhr.status == 200) {
 
         const data = xhr.response;
-        var numTable = data.length;
+        ff= data.length;
         const tableSpace= document.getElementById("tableSpace");
         tableSpace.innerHTML = "";
         console.log(data);
+        let index = 0;
         data.forEach(item => {
-          tableSpace.innerHTML += "<div id=\"divTab-"+item.id+"\"><h3>"+item.table+"</h3><table><thead id=\"table-"+item.id+"\"></thead></table></div>";
+          if(data.length>0){
+            pager[index]= {
+              page: 1,
+              tableId: item.id
+            };
+            
+            tableSpace.innerHTML += "<div class=\"tableDiv\" id=\"divTab-"+item.id+"\"><h3 class=\"centrato\">"+item.table+"</h3><table class=\"borderer\"><thead><tr id=\"thead-"+item.id+"\"></tr></thead><tbody><tr id=\"table-"+item.id+"\"></tr></tbody></table><button id=\"prev-"+item.id+"\" onclick=\"prev("+item.id+")\">prev</button> <button id=\"next-"+item.id+"\" onclick=\"next("+item.id+")\">next</button></div>"; //<tr><th class=\"borderer\">ID</th><th class=\"borderer\">Title</th><th class=\"borderer\">Views</th><th class=\"borderer\">Table</th></tr>
 
-          // document.getElementById("table-"+data.id).innerHTML = data.title;
+            thead = document.getElementById("thead-"+item.id);
+
+            for(var i=0;i<item.th.length;i++){
+              thead.innerHTML += "<th class=\"borderer\">"+ item.th[i] +"</th>";
+            }
+          }else{
+            tableSpace.innerHTML = "no table";
+          }
+
           getAll(item.id);
+          index++;
         });
-
-        // document.getElementById("ciao").innerHTML = data[1].title;
 
     } else {
       console.log(`Error: ${xhr.status}`);
@@ -34,9 +48,16 @@ function getTitle(){
 }
 
 function getAll(tableId){
-  // const numTable = getTitle();
+
+  var page=1;
+  for(i=0;i<ff;i++){
+    if(pager[i].tableId == tableId){
+      page= pager[i].page;
+      break;
+    }
+  }
   const xhr = new XMLHttpRequest();
-  xhr.open("GET", "http://localhost:3000/posts?_filter=table_eq="+tableId+"&_page="+page+"&_limit=100&_sort=views");
+  xhr.open("GET", "http://localhost:3000/posts?table="+tableId+"&_page="+page+"&_limit=1"); //&_sort=views
   xhr.send();
   xhr.responseType = "json";
   xhr.onload = () => {
@@ -45,16 +66,27 @@ function getAll(tableId){
         const data = xhr.response;
         console.log(data);
 
-        // document.getElementById("ciao").innerHTML = data[0].title;
-        // const tbody = HTMLElement()
-
         table = document.getElementById("table-"+tableId);
 
-        table.innerHTML ="";
+        if(table){
+          table.innerHTML ="";
 
-        for(var i=1;i<data.length;i++){
-          table.innerHTML += "<tr><td>"+ data[i].id +"</td><td>"+ data[i].title +"</td><td>"+ data[i].views +"</td></tr>";
-        }
+          if(data.length>0){
+
+            let index = 0;
+            data.forEach(item => {
+
+              table.innerHTML += "<td>"+ item.id +"</td>";
+
+              for(var i=0;i<item.data.length;i++){
+                table.innerHTML += "<td>"+ item.data[i] +"</td>";
+              }
+            });
+          }else{
+            table.innerHTML = "<tr class=\"borderer\"><p class=\"borderer\">ddddd</p></tr>"
+          }
+
+        } 
 
 
     } else {
@@ -63,51 +95,60 @@ function getAll(tableId){
   };
 }
 
-function next(){
-  page= page+1;
-  getAll();
+function next(tableId){
+
+  for(i=0;i<ff;i++){
+    if(pager[i].tableId == tableId){
+      pager[i].page= pager[i].page+1;
+    }
+  }
+
+  getAll(tableId);
 }
 
-function prev(){
-  if(page>1){
-    page= page-1;
-    getAll();
+function prev(tableId){
+
+  for(i=0;i<ff;i++){
+    if(pager[i].tableId == tableId && pager[i].page>1){
+      pager[i].page= pager[i].page-1;
+      getAll(tableId);
+    }
   }
 }
 
 function putt(){
-    // const xhr = new XMLHttpRequest();
-    // xhr.open("POST", "http://localhost:3000/posts");
-    // xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    // const body = JSON.stringify({
-    // title: "Hello World",
-    // views: 200
-    // });
-    // xhr.onload = () => {
-    // if (xhr.readyState == 4 && xhr.status == 201) {
-    //     console.log(JSON.parse(xhr.responseText));
-    // } else {
-    //     console.log(`Error: ${xhr.status}`);
-    // }
-    // };
-    // xhr.send(body);
 
-    const options = {
+    var title = document.getElementById("title").value;
+
+    var views = document.getElementById("views").value;
+
+    var table = document.getElementById("table").value;
+
+    if(title!="" && views!="" && table!=""){
+      debugger;
+      const options = {
         method: 'POST',
         headers: {
         'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            title: "Hello Worl",
-            views: 200
+            title: title,
+            views: views,
+            table: table
         }),
-    };
+      };
 
-    fetch('http://localhost:3000/posts', options)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        getAll();
-      })
-      .catch(error => console.error('Error:', error));
+      fetch('http://localhost:3000/posts', options)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          getAll(table);
+        })
+        .catch(error => console.error('Error:', error));
+
+    }
+    else{
+      console.log("campi mancanti")
+    }
+
 }
